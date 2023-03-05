207bd71685d8f52c693263f62b4380d7b901a4d8
unknown <jaelveramo@yahoo.es>
2023-03-04 22:58:21 -0500

    searcher_20230304_2258

--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -1,8 +1,8 @@
 //var DOMParser = require('dom-parser');
 
 loadProducts = () => {
-    let URLJson = "https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json"
-    let URLXml = "https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml"
+    let URLJson = "https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json";
+    let URLXml = "https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml";
     let templateProductoJson = "";
     let templateProductoXml = "";
     let filtro = document.getElementById("text").value;
@@ -16,85 +16,85 @@ loadProducts = () => {
 
         if (hayFiltro) {
           resultJson = resultJson.filter((el) => (el.name.includes(filtro.toLowerCase()) || el.type.includes(filtro.toLowerCase())));
-        }
+        };
 
         resultJson.forEach(elemento => {
           let {name, price, src, type} = elemento;
 
           templateProductoJson += `
-          <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
-            <div class="card card-blog card-plain">
-              <div class="card-header p-0 mt-n4 mx-3">
-                <a class="d-block shadow-xl border-radius-xl">
-                  <img src="${src}" alt="${name}" class="img-fluid shadow border-radius-xl">
-                </a>
-              </div>
-              <div class="card-body p-3">
-                <p class="mb-0 text-sm">${type}</p>
-                <a href="javascript:;">
-                  <h5>
-                    ${name}
-                  </h5>
-                </a>
-                <p class="mb-4 text-sm">
-                  <b>Price: </b> $ ${price}
-                </p>
-              </div>
-            </div>
-          </div>`
+                  <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
+                    <div class="card card-blog card-plain">
+                      <div class="card-header p-0 mt-n4 mx-3">
+                        <a class="d-block shadow-xl border-radius-xl">
+                          <img src="${src}" alt="${name}" class="img-fluid shadow border-radius-xl">
+                        </a>
+                      </div>
+                      <div class="card-body p-3">
+                        <p class="mb-0 text-sm">${type}</p>
+                        <a href="javascript:;">
+                          <h5>
+                            ${name}
+                          </h5>
+                        </a>
+                        <p class="mb-4 text-sm">
+                          <b>Price: </b> $ ${price}
+                        </p>
+                      </div>
+                    </div>
+                  </div>`;
         });
 
         document.getElementById("productos").innerHTML += templateProductoJson;
       } catch (error) {
         console.log(error);
-      }
-    }
+      };
+    };
 
     let requestXml = async (miUrlXml) => {
       try {
         let responseXml = await fetch(miUrlXml);
         let resultXml = await responseXml.text();
         let xml = (new DOMParser()).parseFromString(resultXml, 'application/xml');
-        let arrProducto = xml.getElementsByTagName("product");
+        let arrProducto = Array.from(xml.getElementsByTagName("product"));
 
         if (hayFiltro) {
-          arrProducto = Array.from(arrProducto).filter((el) => (el.getElementsByTagName("name")[0].innerHTML.includes(filtro.toLowerCase()) || el.getElementsByTagName("type")[0].innerHTML.includes(filtro.toLowerCase())));
-        }
+          arrProducto = arrProducto.filter((el) => (el.getElementsByTagName("name")[0].innerHTML.includes(filtro.toLowerCase()) || el.getElementsByTagName("type")[0].innerHTML.includes(filtro.toLowerCase())));
+        };
 
-        for (let elemento of arrProducto) {
+        arrProducto.forEach(elemento => {
           let name = elemento.getElementsByTagName("name")[0].innerHTML;
           let price = elemento.getElementsByTagName("price")[0].innerHTML;
           let src = elemento.getElementsByTagName("src")[0].innerHTML;
           let type = elemento.getElementsByTagName("type")[0].innerHTML;
 
           templateProductoXml += `
-          <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
-            <div class="card card-blog card-plain">
-              <div class="card-header p-0 mt-n4 mx-3">
-                <a class="d-block shadow-xl border-radius-xl">
-                  <img src="${src}" alt="${name}" class="img-fluid shadow border-radius-xl">
-                </a>
-              </div>
-              <div class="card-body p-3">
-                <p class="mb-0 text-sm">${type}</p>
-                <a href="javascript:;">
-                  <h5>
-                    ${name}
-                  </h5>
-                </a>
-                <p class="mb-4 text-sm">
-                  <b>Price: </b> $ ${price}
-                </p>
-              </div>
-            </div>
-          </div>`
-        };
+                  <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
+                    <div class="card card-blog card-plain">
+                      <div class="card-header p-0 mt-n4 mx-3">
+                        <a class="d-block shadow-xl border-radius-xl">
+                          <img src="${src}" alt="${name}" class="img-fluid shadow border-radius-xl">
+                        </a>
+                      </div>
+                      <div class="card-body p-3">
+                        <p class="mb-0 text-sm">${type}</p>
+                        <a href="javascript:;">
+                          <h5>
+                            ${name}
+                          </h5>
+                        </a>
+                        <p class="mb-4 text-sm">
+                          <b>Price: </b> $ ${price}
+                        </p>
+                      </div>
+                    </div>
+                  </div>`;
+        });
 
         document.getElementById("productos").innerHTML += templateProductoXml;
       } catch (error) {
         console.log(error);
-      }
-    }
+      };
+    };
   
   requestJson(URLJson);
   requestXml(URLXml);
@@ -109,6 +109,7 @@ btnFilter.addEventListener('click', (event) => {
 
   if (hayFiltro === false) {
     window.alert("No ha ingresado filtro");
-  }
+  };
+  
   loadProducts();
 });
